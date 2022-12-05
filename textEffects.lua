aa9511e72cebdbe8e30e5109751e0036ffc86bd3
DoComplement <92898998+DoComplement@users.noreply.github.com>
2022-12-05 01:03:48 -0500

    Update textEffects.lua

--- textEffects.lua
+++ textEffects.lua
@@ -2,7 +2,9 @@
 local rand = loadstring(game:HttpGet("https://raw.githubusercontent.com/DoComplement/Classes/main/random.lua"))()
 local stringToEffect = "This is a random String! WOW!"
 
-local function specialConcat(Table)
+local textEffectLib = {}
+
+textEffectsLib.specialConcat = function(Table)
     local Concatenation = table.create(#Table)
     for _,v in pairs(Table) do
         table.insert(Concatenation, v)
@@ -11,8 +13,7 @@ local function specialConcat(Table)
 end
 
 --[[ popEffect and sumEffect can be consolidated ]]
-
-local function popEffect(Instance, String, Delay)
+textEffectsLib.popEffect = function(Instance, String, Delay)
     Instance.Text = ''
     local spaceString = table.create(#String, ' ')
     String = String:split('')
@@ -20,11 +21,11 @@ local function popEffect(Instance, String, Delay)
     for _,randIndex in ipairs(rand.randSequence(#String)) do
         spaceString[randIndex] = String[randIndex]
         Instance.Text = table.concat(spaceString) 
-        task.wait(Delay)
+        if Delay then task.wait(Delay) end
     end
 end
 
-local function sumEffect(Instance, String, Delay)
+textEffectsLib.sumEffect = function(Instance, String, Delay)
     Instance.Text = ''
     local spaceString = {}
     String = String:split('')
@@ -32,29 +33,68 @@ local function sumEffect(Instance, String, Delay)
     for _,randIndex in ipairs(rand.randSequence(#String)) do
         spaceString[randIndex] = String[randIndex]
         Instance.Text = specialConcat(spaceString) 
-        task.wait(Delay)
+        if Delay then task.wait(Delay) end
     end
 end
 
-local function stringDecryptionEffect(Instance, String, Delay)
-    local randString = rand.randString(#String):split('')
-    local referenceTable = rand.randSequence(#String)
+textEffectsLib.sweepEffect = function(Instance, String, inOut, Delay)
+	
+end
+
+--[[ Types: Base, , Random, Sweep (in, out, left, right) ]]
+--[[
+    The current module is rather pleasing, but there are a few things i would like to add:
+    1) add extra characters to the randString and delete them accordingly to the end of the sequence
+    2) make the encrypted characters rotate other random characters until they are fixed (could be a separate mode or toggle)
+
+]]
+textEffectsLib.decryptionEffect = function(Instance, String, Delay)
+    local Size = #String
+    Instance.Text = rand.randString(Size)
+    local randString, refereceIndex = Instance.Text:split('')
+    local referenceTable = rand.randSequence(Size)
     String = String:split('')
-    Instance.Text = table.concat(randString)
     
-    for _,randIndex in ipairs(referenceTable) do 
-        for count = 1,math.random(1, 10) do
-            randString[referenceTable[math.random(1, #referenceTable)]] = rand.AlphaBET[math.random(1, 64)]
-        end
-        randString[randIndex] = table.remove(referenceTable, math.random(1, #referenceTable))
-        Instance.Text = table.concat(randString)
-        task.wait(Delay)
-    end
+    for Index = 1, Size do -- Type = Random
+		for count = 1,math.random(10, 50) do
+			randString[referenceTable[math.random(1, #referenceTable)]] = rand.AlphaBET[math.random(1, 64)]
+			Instance.Text = table.concat(randString)
+		end
+		referenceIndex = math.random(1, #referenceTable)
+		randString[referenceTable[referenceIndex]] = String[table.remove(referenceTable, referenceIndex)]
+		Instance.Text = table.concat(randString)
+        task.wait(0.05 + math.exp(Index - Size - 1))
+    end 
 end
 
 
 
 --[[
-    local TextLabel = game:GetService("Players").LocalPlayer.PlayerGui.TestGui.Frame.TextLabel
-    sumEffect(TextLabel, stringToEffect, 0.1)
+
+    local ScreenGui = Instance.new("ScreenGui")
+    local Frame = Instance.new("Frame")
+    local TextLabel = Instance.new("TextLabel")
+    
+    --Properties:
+    
+    ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    ScreenGui.Name = "TestGui"
+    
+    Frame.Parent = ScreenGui
+    Frame.BackgroundColor3 = Color3.fromRGB(125, 125, 125)
+    Frame.BackgroundTransparency = 0.500
+    Frame.Position = UDim2.new(0.61585784, 0, 0.107231915, 0)
+    Frame.Size = UDim2.new(0, 403, 0, 73)
+    
+    TextLabel.Parent = Frame
+    TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    TextLabel.Position = UDim2.new(0.024813896, 0, 0.150684938, 0)
+    TextLabel.Size = UDim2.new(0, 382, 0, 50)
+    TextLabel.Font = Enum.Font.SourceSans
+    TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+    TextLabel.TextSize = 14.000
+
+    textEffectsLib.decryptionEffect(TextLabel, stringToEffect)
+
 ]]
