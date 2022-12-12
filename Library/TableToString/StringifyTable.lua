9e2b8dd4827a6dab079fed4b2816f68cebe4fceb
DoComplement <92898998+DoComplement@users.noreply.github.com>
2022-12-12 04:06:08 -0500

    Update StringifyTable.lua

--- Library/TableToString/StringifyTable.lua
+++ Library/TableToString/StringifyTable.lua
@@ -1,32 +1,32 @@
 if getgenv().ConvertTable then getfenv(getgenv().ConvertTable).script:Destroy() end
-getgenv().ConvertTable = function(Table, Title, Sort)
-    assert(type(Table) == "table", "ERROR! Input table is not a valid table.")
+getgenv().ConvertTable = function(Table, MainTitle, Sort)
+	assert(type(Table) == "table", "ERROR! Input table is not a valid table.")
     
-    local function GetIndices(Table)
-        local Indices = {}
-        for Index,Value in next, Table do 
-            table.insert(Indices, Index)
-        end
+    	local function GetIndices(Table)
+        	local Indices = {}
+        	for Index,Value in next, Table do 
+			table.insert(Indices, Index)
+        	end
         
-        if Sort ~= nil then
-            if Sort then
-                table.sort(Indices, function(A,B) return A < B end) -- Ascending
-            else
-                table.sort(Indices, function(A,B) return B < A end) -- Descending
-            end
-        end
+		if Sort ~= nil then
+			if Sort then
+				table.sort(Indices, function(A,B) return A < B end) -- Ascending
+			else
+				table.sort(Indices, function(A,B) return B < A end) -- Descending
+			end
+		end
         
-        if Indices[1] then -- if Table is non-empty
-            return Indices,Table[Indices[table.getn(Indices)]] -- Array and Last Element
-        end
-    end
+		if Indices[1] then -- if Table is non-empty
+			return Indices,Table[Indices[table.getn(Indices)]] -- Array and Last Element
+		end
+	end
     
-    local function concatTable(Table) -- table is created using table.insert
-        for Index, Value in next, Table do -- thus, ipairs can be used
-            Table[Index] = tostring(Value)
-        end
-        return table.concat(Table)
-    end
+	local function concatTable(Table) -- table is created using table.insert
+		for Index, Value in next, Table do -- thus, ipairs can be used
+			Table[Index] = tostring(Value)
+		end
+		return table.concat(Table)
+	end
     
 	local Types = {
 		["number"] = '\n',
@@ -34,46 +34,51 @@ getgenv().ConvertTable = function(Table, Title, Sort)
 		["table"] = '\n'
 	}
 	
-    local function formatTable(UpperEntity, Title, Tab, notLast)
-        
-        local StringTable,EntityType = {},type(UpperEntity)
-        local TypeFormat = Types[EntityType] or " -->\t"..EntityType..'\n'
-        local Indices,Last
-        
-        if EntityType == "table" then 
-            Indices,Last = GetIndices(UpperEntity) 
-        end
+    	local function formatTable(UpperEntity, Title, Tab, notLast)	
 		
-	if #Tab > 0 then
-		if type(Title) == "string" then 
-			Title = "[\""..Title.."\"]" 
-		else
-			Title = '['..tostring(Title)..']'
-		end
-	end
+		local StringTable,EntityType = {},type(UpperEntity)
+		local TypeFormat = Types[EntityType] or " -->\t"..EntityType..'\n'
+		local Indices,Last
         
-        if EntityType == "table" then
-		if Title then
-			table.insert(StringTable, Tab..Title.." = {\n")
-		else
-			table.insert(StringTable, "{\n")
+		if EntityType == "table" then
+			Indices,Last = GetIndices(UpperEntity)
 		end
-            	for _,Index in ipairs(Indices) do
-                	local Entity = UpperEntity[Index]
-                	table.insert(StringTable, formatTable(Entity, Index, Tab..'\t', Entity ~= Last))
-            	end
-            	table.insert(StringTable, Tab.."}")
-        else
-            	if EntityType == "string" then UpperEntity = "\""..UpperEntity.."\"" end
-            	table.insert(StringTable, Tab..Title.." = "..tostring(UpperEntity))
-        end
+                
+		if #Tab ~= 0 then
+			if type(Title) == "string" then 
+				Title = "[\""..Title.."\"]" 
+			else
+				Title = '['..tostring(Title)..']'
+			end
+			if EntityType == "table" then
+				table.insert(StringTable, Tab..Title.." = {\n")
+			elseif EntityType == "string" then
+				UpperEntity = "\""..UpperEntity.."\""
+			end
+		else
+			if Title then
+				table.insert(StringTable, "local "..Title.." = {\n")
+			else
+				table.insert(StringTable, "{\n")
+			end
+		end
+        
+        	if EntityType == "table" then
+			for _,Index in ipairs(Indices) do
+				local Entity = UpperEntity[Index]
+				table.insert(StringTable, formatTable(Entity, Index, Tab..'\t', Entity ~= Last))
+			end
+			table.insert(StringTable, Tab.."}")
+        	else
+            		table.insert(StringTable, Tab..Title.." = "..tostring(UpperEntity))
+        	end
       
-        if notLast then table.insert(StringTable, ',') end 
-	if #Tab > 0 then table.insert(StringTable, TypeFormat) end
-        return concatTable(StringTable)
-    end
-
-    return formatTable(Table, Title or "ConvertedTable", '', false)
+        	if notLast then table.insert(StringTable, ',') end 
+		if #Tab > 0 then table.insert(StringTable, TypeFormat) end
+        	return table.concat(StringTable)
+    	end
+	
+    	return formatTable(Table, MainTitle, '', false)
 end
 
 print("<string> getgenv().ConvertTable(<table> Table, <string> nil or Table_Name, <boolean> Sort | true -> Ascending, false -> Descending, nil -> no sort)")
