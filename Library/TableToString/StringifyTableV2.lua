5df7938cb0dbf2c471f262599216ae756cd5d533
DoComplement <92898998+DoComplement@users.noreply.github.com>
2022-12-18 09:06:41 -0500

    Update StringifyTableV2.lua

--- Library/TableToString/StringifyTableV2.lua
+++ Library/TableToString/StringifyTableV2.lua
@@ -1,15 +1,15 @@
 if getgenv().StringifyTable then getfenv(getgenv().StringifyTable).script:Destroy() end
 getgenv().StringifyTable = function(Table, MainTitle, Sort)
-    assert(type(Table) == "table", "ERROR! Input table is not a valid table.")
+	assert(type(Table) == "table", "ERROR! Input table is not a valid table.")
     
-    local function GetIndices(Table)
-        local Indices = {}
-        for Index,Value in next, Table do 
-            table.insert(Indices, Index)
-        end
+	local function GetIndices(Table)
+        	local Indices = {}
+        	for Index,Value in next, Table do 
+        		table.insert(Indices, Index)
+		end
         
 		if Sort ~= nil then
-            if Sort then
+			if Sort then
 				table.sort(Indices, function(A,B) return A < B end) -- Ascending
 			else
 				table.sort(Indices, function(A,B) return B < A end) -- Descending
@@ -22,13 +22,13 @@ getgenv().StringifyTable = function(Table, MainTitle, Sort)
 	local Tables,Indices,Last,Model = {}
 	local function formatTable(Entity, Index, Tab, notLast)	
 		
-        _,Model = pcall(tostring, Entity)
+	        _,Model = pcall(tostring, Entity)
 		Index = Index and (type(Index) == "string" and Tab.."[\""..Index.."\"] = " or Tab..'['..tostring(Index).."] = ") or ''
 		
 		if type(Entity) == "table" then
 			Indices,Last = GetIndices(Entity)
-            if table.find(Tables, Entity) == nil then -- checking for repeat-nested tables
-                table.insert(Tables, Entity)
+			if table.find(Tables, Entity) == nil then -- checking for repeat-nested tables
+                		table.insert(Tables, Entity)
 				local StringTable = {}
 				if table.getn(Indices) ~= 0 then
 					for _,Index in ipairs(Indices) do
