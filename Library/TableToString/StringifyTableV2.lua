2579c2f88ad0c6e4113dd0d3eb0d48e48b649a8c
DoComplement <92898998+DoComplement@users.noreply.github.com>
2022-12-18 07:40:52 -0500

    Update StringifyTableV2.lua

--- Library/TableToString/StringifyTableV2.lua
+++ Library/TableToString/StringifyTableV2.lua
@@ -20,29 +20,29 @@ getgenv().StringifyTable = function(Table, MainTitle, Sort)
 	end
     
 	local Tables = {}
-	local function formatTable(UpperEntity, Title, Tab, notLast)	
+	local function formatTable(Entity, Index, Tab, notLast)	
 		
-		local Index = Title and (type(Title) == "string" and Tab.."[\""..Title.."\"] = " or Tab..'['..tostring(Title).."] = ") or ''
+		Index = Index and (type(Index) == "string" and Tab.."[\""..Index.."\"] = " or Tab..'['..tostring(Index).."] = ") or ''
 		
-		if type(UpperEntity) == "table" then
-			Indices,Last = GetIndices(UpperEntity)
-            if table.find(Tables, UpperEntity) == nil then -- checking for repeat-nested tables
-                table.insert(Tables, UpperEntity)
+		if type(Entity) == "table" then
+			Indices,Last = GetIndices(Entity)
+            if table.find(Tables, Entity) == nil then -- checking for repeat-nested tables
+                table.insert(Tables, Entity)
 				local StringTable = {}
 				if table.getn(Indices) ~= 0 then
 					for _,Index in ipairs(Indices) do
-						local Entity = UpperEntity[Index]
-						table.insert(StringTable, formatTable(Entity, Index, Tab..'\t', Last ~= Index))
+						local Element = Entity[Index]
+						table.insert(StringTable, formatTable(Element, Index, Tab..'\t', Last ~= Element))
 					end
-					return Index.."{ \t-- "..tostring(UpperEntity)..'\n'..table.concat(StringTable)..Tab..(notLast and "},\n" or "}\n")
+					return Index.."{ \t-- "..tostring(Entity)..'\n'..table.concat(StringTable)..Tab..(notLast and "},\n" or "}\n")
 				else 
 					return Index..(notLast and "{},\n" or "{}\n") 
 				end
 			else
-				return Index.."\"Repeated Table\""..(notLast and ',' or '').."\t-- "..tostring(UpperEntity)..'\n'
+				return Index.."\"Repeated Table\""..(notLast and ',' or '').."\t-- "..tostring(Entity)..'\n'
 			end
 		else
-			return Index..tostring(UpperEntity)..(notLast and ",\n" or '\n')
+			return Index..tostring(Entity)..(notLast and ",\n" or '\n')
 		end
 	end	
 	
