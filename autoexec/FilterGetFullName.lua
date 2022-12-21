730d261abe89a8bb66b63719a6212568598bd539
DoComplement <92898998+DoComplement@users.noreply.github.com>
2022-12-21 14:59:01 -0500

    Update FilterGetFullName.lua

--- autoexec/FilterGetFullName.lua
+++ autoexec/FilterGetFullName.lua
@@ -1,8 +1,8 @@
-game.Loaded:Wait()
-local Username = game:GetService("Players").LocalPlayer.Name
+if Game:IsLoaded() == false then Game.Loaded:Wait() end
+local Username = Game:GetService("Players").LocalPlayer.Name
 local OldNamecall
 
-OldNamecall = hookmetamethod(game, "__namecall", function(Self, ...)
+OldNamecall = hookmetamethod(Game, "__namecall", function(Self, ...)
     if checkcaller() and getmetamethod() == "GetFullName" then
         local Filter = OldNamecall(Self, ...):gsub(Username, "LocalPlayer") -- removes username from string
         return Filter
