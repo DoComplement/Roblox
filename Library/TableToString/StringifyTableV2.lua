ad529ae676dfccc5f1a4f269d317148c092941eb
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-01-14 16:03:47 -0500

    Update StringifyTableV2.lua

--- Library/TableToString/StringifyTableV2.lua
+++ Library/TableToString/StringifyTableV2.lua
@@ -1,54 +1,54 @@
-if Game:IsLoaded() == false then Game.Loaded:Wait() end
+if Game:IsLoaded() == false then Game.Loaded:Wait() end;
 
-if getgenv().t2s ~= nil then getfenv(getgenv().t2s).script:Destroy() end
+if getgenv().t2s ~= nil then getfenv(getgenv().t2s).script:Destroy() end;
 getgenv().t2s = function(Table, MainTitle, Sort)
-	assert(type(Table) == "table", "ERROR! Input table is not a valid table.")
+	assert(type(Table) == "table", "ERROR! Input table is not a valid table.");
     
 	local function GetIndices(Table)
-        	local Indices = {}
+        	local Indices = {};
         	for Index,Value in next, Table do 
-        		table.insert(Indices, Index)
-		end
+        		table.insert(Indices, Index);
+		end;
         
 		if Sort ~= nil then
 			if Sort then
-				table.sort(Indices, function(A,B) return A < B end) -- Ascending
+				table.sort(Indices, function(A,B) return A < B end); -- Ascending
 			else
-				table.sort(Indices, function(A,B) return B < A end) -- Descending
-			end
-		end
+				table.sort(Indices, function(A,B) return B < A end); -- Descending
+			end;
+		end;
         
-		return Indices,Table[Indices[table.getn(Indices)]] -- Array and Last Element
-	end
+		return Indices,Table[Indices[table.getn(Indices)]]; -- Array and Last Element
+	end;
     
-	local Tables = {}
+	local Tables = {};
 	local function formatTable(Entity, Index, Tab, notLast)	
 		
-		local _,Model = xpcall(function() return type(Entity) == "string" and '\"'..Entity..'\"' or tostring(Entity) end, tostring, Entity)
-		Index = Index and (type(Index) == "string" and Tab.."[\""..Index.."\"] = " or Tab..'['..tostring(Index).."] = ") or ''
+		local _,Model = xpcall(function() return type(Entity) == "string" and '\"'..Entity..'\"' or tostring(Entity) end, tostring, Entity);
+		Index = Index and (type(Index) == "string" and Tab.."[\""..Index.."\"] = " or Tab..'['..tostring(Index).."] = ") or '';
 		
 		if type(Entity) == "table" then
-			local Indices,Last = GetIndices(Entity)
+			local Indices,Last = GetIndices(Entity);
 			if table.find(Tables, Entity) == nil then -- checking for repeat-nested tables
-                		table.insert(Tables, Entity)
-				local StringTable = {}
+                		table.insert(Tables, Entity);
+				local StringTable = {};
 				if table.getn(Indices) ~= 0 then
 					for _,Index in ipairs(Indices) do
-						local Element = Entity[Index]
-						table.insert(StringTable, formatTable(Element, Index, Tab..'\t', Last ~= Element))
-					end
-					return Index.."{ \t-- "..Model..'\n'..table.concat(StringTable)..Tab..(notLast and "},\n" or "}\n")
+						local Element = Entity[Index;
+						table.insert(StringTable, formatTable(Element, Index, Tab..'\t', Last ~= Element));
+					end;
+					return Index.."{ \t-- "..Model..'\n'..table.concat(StringTable)..Tab..(notLast and "},\n" or "}\n");
 				else 
-					return Index..(notLast and "{},\n" or "{}\n") 
-				end
+					return Index..(notLast and "{},\n" or "{}\n");
+				end;
 			else
-				return Index.."\"Repeated Table\",\t-- "..Model..'\n'
-			end
+				return Index.."\"Repeated Table\",\t-- "..Model..'\n';
+			end;
 		else
-			return Index..Model..(notLast and ",\n" or '\n')
-		end
-	end	
+			return Index..Model..(notLast and ",\n" or '\n');
+		end;
+	end;
 	
-	return (MainTitle and "local ".. MainTitle.." = " or '')..formatTable(Table, nil, '')
-end
-print("<string> getgenv().t2s(<tuple> Table, <string> Title, <boolean> Sort)")
+	return (MainTitle and "local ".. MainTitle.." = " or '')..formatTable(Table, nil, '');
+end;
+print("<string> getgenv().t2s(<tuple> Table, <string> Title, <boolean> Sort)");
