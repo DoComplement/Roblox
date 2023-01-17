28a725e596521acd7162afca8190a3402b0bcc88
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-01-16 22:00:44 -0500

    Create HookSolution.lua

---
+++ Scripts/HookSolution.lua
@@ -0,0 +1,42 @@
+-- this method is directly influenced from 0866 on v3rmillion
+
+_G.hooks = _G.hooks or {};
+
+local function createhook(fn)
+    local function proxy(...)
+        local hook = _G.hooks[debug.info(1, 'f')];
+        return (hook.replacement or hook.original)(...);
+    end;
+    
+    _G.hooks[fn] = {    
+        original = hookfunction(fn, proxy)  
+    };
+    
+    return function(replacement)
+        _G.hooks[fn].replacement = replacement;
+    end;
+end;
+
+-- Examples
+--[[
+local tobehooked = function(...)
+    print(...);
+end;
+
+local a,b = 1,2; -- upvalues
+local tohook = function(...)
+    print(a,b,...);
+end;
+
+-- Hooking methods
+local hook = createhook(tobehooked);
+hook(tohook);   -- hooking tobehooked with tohook
+tobehooked(3, 4, 5); -- 1, 2, 3, 4, 5
+
+hook(nil);  -- unhook
+tobehooked(1, 2, 3); -- 1, 2, 3
+
+-- if unhooking is not significant
+hook(tobehooked)(tohook);
+tobehooked(3, 4, 5); -- 1, 2, 3, 4, 5
+]]
