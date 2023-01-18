52171a9f1d3a1c1b4c7dcc8285395b306ac1ad27
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-01-18 03:55:27 -0500

    Update ClearDevConsole.lua

--- Scripts/ClearDevConsole.lua
+++ Scripts/ClearDevConsole.lua
@@ -1,26 +1,22 @@
 -- messages after each clear will reappear every time the dev console is re-opened
 -- the function can be bound to a keybind to clear the console on some command as well as setting the command to F9 so the console always opens cleared
 
-local function OpenDevConsole()
-    local Box = game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar
-    
-    game:GetService("VirtualUser"):CaptureController();
-    Box:SetTextFromInput("/console");
-    Box:CaptureFocus();
-    game:GetService("VirtualUser"):TypeKey("0x0D"); -- Press Enter Key
-end;
+local function OpenDevConsole(DevConsoleUI)
+    if DevConsoleUI:FindFirstChild("MainView") == nil then
+        local Box = game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar
 
-local function GetConsole()
-    if game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI:FindFirstChild("MainView") == nil then
-        OpenDevConsole();
+        game:GetService("VirtualUser"):CaptureController();
+        Box:SetTextFromInput("/console");
+        Box:CaptureFocus();
+        game:GetService("VirtualUser"):TypeKey("0x0D"); -- Press Enter Key
     end;
-    return game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI:WaitForChild("MainView");
+    return DevConsoleUI:WaitForChild("MainView");
 end;
 
 return function()
-    for _,Instance in ipairs(GetConsole().ClientLog:GetChildren()) do
+    for _,Instance in ipairs(OpenDevConsole(game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI).ClientLog:GetChildren()) do
         if tonumber(Instance.Name) ~= nil then
-            Instance:Destroy();
+            Instance.Visible = false; 
         end;
     end;
 end;
