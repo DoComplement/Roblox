7f39fab1920702fb5d871399a02d03027886638d
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-01-23 21:06:05 -0500

    Update Rand.lua

--- Library/RandLib/Rand.lua
+++ Library/RandLib/Rand.lua
@@ -1,13 +1,14 @@
 local rand = {};
 
 function rand:createLinearTable(Length)
-	local Table = table.create(Length, 1);
+	local Table = table.create(Length, 0);
+	for i,_ in ipairs(Table) do 
 	for i = 2, Length do Table[i] = i end;
 	return Table;
 end;
 
 function rand:randSequence(Length)
-	local Sequence = table.create(Length, '\0');
+	local Sequence = table.create(Length, '');
 	local referenceTable = rand:createLinearTable(Length);
 	for i=1,Length do
 		Sequence[i] = table.remove(referenceTable, math.random(1, table.getn(referenceTable)));
@@ -18,7 +19,7 @@ end;
 -- must be string or array (dictionary)
 function rand:Randomize(Entity, Concat)
 	if type(Entity) == "string" then Entity = Entity:split('') end;
-	local Table = table.create(table.getn(Entity), '\0');
+	local Table = table.create(table.getn(Entity), '');
 	for Idx, randIdx in ipairs(rand:randSequence(table.getn(Entity))) do
 		Table[Idx] = Entity[randIdx];
 	end;
@@ -26,17 +27,19 @@ function rand:Randomize(Entity, Concat)
 end;
 
 function rand:Char()
-	
+	return rand.AlphaBET[math.random(1, 64)];
 end
 
 rand.AlphaBET = rand:Randomize("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789_ ");
 
 function rand:randString(Length)
+	assert(Length > 0);
+	rand:updateAlphabet();
 	if Length >= 200 then
-		return table.concat(table.create(Length, '\0')):gsub('.', function() return rand.AlphaBET[math.random(1, 64)] end);
+		return table.concat(table.create(Length, '')):gsub('.', rand.Char);
 	else
 		local String = '';
-		for _ = 1, Length do String = String .. rand.AlphaBET[math.random(1, 64)] end;
+		for _=1,Length do String = String .. rand.AlphaBET[math.random(1, 64)] end;
 		return String;
 	end;
 end;
