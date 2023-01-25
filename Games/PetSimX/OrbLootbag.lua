9ec9feb7a6d682c369fcf3ef0b76dd289040cb11
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-01-25 00:05:01 -0500

    Update OrbLootbag.lua

--- Games/PetSimX/OrbLootbag.lua
+++ Games/PetSimX/OrbLootbag.lua
@@ -4,21 +4,21 @@ local PrimaryPart = game:GetService("Players").LocalPlayer.Character.PrimaryPart
 local Connections = {}
 
 for _,Orb in ipairs(Orbs:GetChildren()) do
-	Orb.CFrame = PrimaryPart.CFrame
-end
+	Orb.CFrame = PrimaryPart.CFrame;
+end;
 
 for _,Lootbag in ipairs(Lootbags:GetChildren()) do
-	Lootbag.CFrame = PrimaryPart.CFrame
-end
+	Lootbag.CFrame = PrimaryPart.CFrame;
+end;
 
 Connections["OrbConnection"] = Orbs.ChildAdded:Connect(function(Orb)
-	while Orb.Parent and wait() do
+	while Orb.Parent ~= nil and task.wait() do
 		Orb.CFrame = PrimaryPart.CFrame
 	end
 end)
 
 Connections["LootbagConnection"] = Lootbags.ChildAdded:Connect(function(Lootbag)
-	while Lootbag.Parent and wait() do
+	while Lootbag.Parent ~= nil and task.wait() do
 		Lootbag.CFrame = PrimaryPart.CFrame
 	end
 end)  
