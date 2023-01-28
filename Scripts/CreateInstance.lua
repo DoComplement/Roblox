38c676c2bbd6a58b961608d45b8ba9475217fb6e
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-01-28 04:59:33 -0500

    Update CreateInstance.lua

--- Scripts/CreateInstance.lua
+++ Scripts/CreateInstance.lua
@@ -1,12 +1,11 @@
-local CreateInstance
+local CreateInstance;
 CreateInstance = hookfunction(Instance.new, function(ObjectType, Properties)
-	if checkcaller() and type(Properties) == "table" then
-		local Instance = CreateInstance(ObjectType)
-		for Attribute, Value in next, Properties do
-			Instance[Attribute] = Value
-		end
-		return Instance
-	else
-		return CreateInstance(ObjectType, Properties) 
-	end
-end)
+	if checkcaller() == false or type(Properties) ~= "table" then
+		return CreateInstance(ObjectType, Properties);
+	end;
+	local Instance = CreateInstance(ObjectType);
+	for Attribute, Value in next, Properties do
+		Instance[Attribute] = Value;
+	end;
+	return Instance;
+end);
