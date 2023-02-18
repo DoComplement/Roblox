38ce37f8d8c70899a58e6748fc9d39f989c3924d
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-02-18 03:03:52 -0500

    Create LocalData.lua

---
+++ Games/WheatFarmingSimulator/LocalData.lua
@@ -0,0 +1,54 @@
+{ 	-- table: 0x000000001ce84bd8
+	["Wheat"] = 0;
+	["TotalWheat"] = 389529;
+	["Pets"] = { 	-- table: 0x0000000061f2a360
+		[1] = { 	-- table: 0x00000000adcbd4e0
+			[1] = "Skeleton Deer";
+			[2] = 3816;
+		};
+		[2] = { 	-- table: 0x000000004e8e6e00
+			[1] = "Snowman";
+			[2] = 5836;
+		};
+		[3] = { 	-- table: 0x00000000204c2120
+			[1] = "Donut";
+			[2] = 8418;
+		};
+		[4] = { 	-- table: 0x000000005c7cfd00
+			[1] = "Spikeball";
+			[2] = 9330;
+		};
+	};
+	["Settings"] = { 	-- table: 0x00000000d7a91840
+		["UpgradeIndicator"] = true;
+		["SFX"] = 100;
+		["AutoPurchase"] = false;
+		["UIScale"] = 1;
+		["Music"] = 100;
+		["ButtonClick"] = true;
+		["Trades"] = true;
+	};
+	["TotalPetsTamed"] = 6;
+	["EquippedHarvester"] = "Iron";
+	["Playtime"] = 11799;
+	["EquippedPets"] = { 	-- table: 0x00000000ec919d20
+		[1] = 3816;
+		[2] = 5836;
+		[3] = 8418;
+		[4] = 9330;
+	};
+	["HarvesterLevels"] = { 	-- table: 0x000000005c0eefc0
+		["Bronze"] = 10;
+		["Wooden"] = 10;
+		["Iron"] = 10;
+	};
+	["Rebirths"] = 0;
+	["Codes"] = {};
+	["MegaRebirths"] = 0;
+	["TotalTrades"] = 0;
+	["TotalTamingCrystals"] = 689;
+	["HarvesterViewing"] = 4;
+	["TamingCrystals"] = 479;
+	["Gold"] = 2324969.6300000087;
+	["TotalGold"] = 5482886.630000118;
+};
