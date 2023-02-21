73da882987bbc39750d69e7d8f85a3342825bdf8
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-02-20 20:05:14 -0500

    Update AutoRejoin_Farm.lua

--- Games/SwordSimulator/Main/AutoRejoin_Farm.lua
+++ Games/SwordSimulator/Main/AutoRejoin_Farm.lua
@@ -27,36 +27,47 @@ LocalPlayer.CharacterAdded:Connect(function(Character)
 	PrimaryPart = Character:WaitForChild("HumanoidRootPart");
 end);
 
+local TempCFrames = {
+	["Autumn Zone Boss"] = CFrame.new(3508.42798, 36.5313873, 12369.9121, -0.945489943, -9.52416528e-08, 0.325651348, -1.07699051e-07, 1, -2.02262811e-08, -0.325651348, -5.41960858e-08, -0.945489943);
+	["Autumn Egg 2"] = CFrame.new(3527.39844, 30.7223492, 12150.4932, -0.308820128, -9.9491011e-08, 0.951120436, -4.06626661e-08, 1, 9.14012119e-08, -0.951120436, -1.04485594e-08, -0.308820128);
+	["Event Egg"] = CFrame.new(74.7213516, 25.8495693, -106.706306, 0.286026955, -2.58336925e-08, 0.958221555, 9.23232193e-08, 1, -5.98229799e-10, -0.958221555, 8.86372078e-08, 0.286026955);
+	["Christmas Zone 2 Egg 2"] = CFrame.new();
+}
+
 task.defer(function()
---	PrimaryPart.CFrame = CFrame.new(3508.42798, 36.5313873, 12369.9121, -0.945489943, -9.52416528e-08, 0.325651348, -1.07699051e-07, 1, -2.02262811e-08, -0.325651348, -5.41960858e-08, -0.945489943); -- Autumn Zone Boss
---	PrimaryPart.CFrame = CFrame.new(3527.39844, 30.7223492, 12150.4932, -0.308820128, -9.9491011e-08, 0.951120436, -4.06626661e-08, 1, 9.14012119e-08, -0.951120436, -1.04485594e-08, -0.308820128); -- Autumn Zone Egg 2;
-	PrimaryPart.CFrame = CFrame.new(74.7213516, 25.8495693, -106.706306, 0.286026955, -2.58336925e-08, 0.958221555, 9.23232193e-08, 1, -5.98229799e-10, -0.958221555, 8.86372078e-08, 0.286026955); -- Event Egg;
+	PrimaryPart.CFrame = TempCFrames["Autumn Egg 2"]; -- Change here (will have to change getgenv().EGG accordingly
     PrimaryPart.Anchored = true;
     task.wait(3);
     PrimaryPart.Anchored = false;
 end);
 
 -- Anti AFK
-local Mouse = LocalPlayer:GetMouse();
-LocalPlayer.Idled:connect(function()	
-	task.wait(math.random(1, 45));
-	
-	game:GetService("VirtualUser"):CaptureController();
-	game:GetService("VirtualUser"):ClickButton2(Vector2.new(Mouse.X, Mouse.Y));
-end);
+if getconnections~=nil then
+	for _,v in ipairs(getconnections(Loca.Player.Idled)) do
+		v:Disable();
+	end;
+else
+	local Mouse = LocalPlayer:GetMouse();
+	local VirtualUser = game:GetService("VirtualUser");
+	LocalPlayer.Idled:connect(function()	
+		task.wait(math.random(1, 45));
 
-
+		VirtualUser:CaptureController();
+		VirtualUser:ClickButton2(Vector2.new(Mouse.X, Mouse.Y));
+	end);
+end;
 
 --					Auto Egg Hatch Begin						
 local EggEvent = game:GetService("ReplicatedStorage").Remotes.Gameplay.RequestPetPurchase;
 local DungeonHandler = LocalPlayer.PlayerScripts.PlayerHandler.Miscallenious.DungeonHandler;
 local Bvent = Instance.new("BindableEvent");
+getgenv().EGG = "Event Egg"; -- change to what is desired
 
 local counter = (os.time() + 3)%10;
 Bvent.Event:Connect(function() 
 	if os.time()%10 ~= counter then return; end;
 	counter = (counter + 3)%10;
-	EggEvent:InvokeServer("Event Egg", "Hatch3"); 
+	EggEvent:InvokeServer(EGG, "Hatch3");
 end);
 
 local f=nil;f = hookfunction(os.time, function()
