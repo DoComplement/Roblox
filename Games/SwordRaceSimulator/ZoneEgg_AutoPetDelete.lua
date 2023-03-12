848984898fd3d65e28c568bafbcd83069569d1f8
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-03-12 12:19:13 -0400

    Update ZoneEgg_AutoPetDelete.lua

--- Games/SwordRaceSimulator/ZoneEgg_AutoPetDelete.lua
+++ Games/SwordRaceSimulator/ZoneEgg_AutoPetDelete.lua
@@ -3,16 +3,6 @@ optimize the scope
 optimize the script itself to achieve the multiple tasks efficiently
 ]]
 
-
--- initialize zone data table
-local Zones = (function(Zones)
-    for id,details in next,require(game.ReplicatedStorage.Modules.LocalConfig.WorldConfig) do
-        Zones.zones[details.WorldName]=id;
-        Zones[id]={};
-    end;
-    return Zones;
-end)({zones={}});
-
 -- format zone data
 local PetConfig = require(game.ReplicatedStorage.Modules.LocalConfig.PetConfig);
 local floor,conversion=math.floor,{
@@ -33,29 +23,20 @@ end;
 
 local DeleteToggles = {};
 local function ToggleAutoPetDelete(Deleting1,Deleting2,Power)
-    
-    -- Unnecessarily called when Deleting1.Visible is changed
-    Deleting2:GetPropertyChangedSignal("Visible"):Connect(function()
-        Deleting1.Visible = Deleting2.Visible;
+    Deleting2.Parent.MouseButton1Click:Connect(function()
+		Deleting1.Visible = not Deleting2.Visible; -- implicitely calls the signal below
     end);
     
-    return function()
-        Deleting2.Visible   = Deleting1.Visible;
-        DeleteToggles[Power]= Deleting2.Visible;
-    end;
+    Deleting1:GetPropertyChangedSignal("Visible"):Connect(function()
+        Deleting2.Visible = Deleting1.Visible;
+        DeleteToggles[Power] = Deleting1.Visible;
+    end);
 end;
 
 local DrawUI = game:GetService("Players").LocalPlayer.PlayerGui.DrawUI;
-local DeletePet = game:GetService("ReplicatedStorage").Remotes.RE_PetDelete;
+local DeletePet = game.ReplicatedStorage.Remotes.RE_PetDelete;
 local List = game:GetService("Players").LocalPlayer.PlayerGui.MainUI.UI.CenterMenu.Index.PetsContainer.List;
-
-local function ConnectAutoDeleteList(EggList)
-    for _,Pet in ipairs(EggList.List.Pets:GetChildren()) do
-        if Pet:IsA("TextButton") then
-            Pet.Deleting:GetPropertyChangedSignal("Visible"):Connect(ToggleAutoPetDelete(Pet.Deleting,List[EggList.Name].List[Pet.Name].Info.Deleting,Abbreviate(PetConfig[Pet.Name].Power)));
-        end;
-    end;
-end;
+local Signals = {};
 
 local function ConnectEggIndices(Name,Eggs,Count,Total)
     local function Update()
@@ -67,7 +48,7 @@ local function ConnectEggIndices(Name,Eggs,Count,Total)
     for idx,Pet in ipairs(Eggs) do
         if idx==1 then continue end;
         local Deleting,Power = Pet.Info.Deleting,Abbreviate(PetConfig[Pet.Name].Power);
-        Pet.Info.MouseButton1Click:Connect(function()
+        Signals[Pet.Name] = Pet.Info.MouseButton1Click:Connect(function()
             Deleting.Visible=not Deleting.Visible;
             DeleteToggles[Power]=Deleting.Visible;
         end);
@@ -79,6 +60,15 @@ local function ConnectEggIndices(Name,Eggs,Count,Total)
     Count=Total-Count;
     print(Name,Count..'/'..Total);
 end;
+
+-- initialize zone data table
+local Zones = (function(Zones)
+    for id,details in next,require(game.ReplicatedStorage.Modules.LocalConfig.WorldConfig) do
+        Zones.zones[details.WorldName]=id;
+        Zones[id]={};
+    end;
+    return Zones;
+end)({zones={}});
 
 for Egg,Data in next,require(game.ReplicatedStorage.Modules.LocalConfig.DrawConfig) do
     if Data.Type==1 then
@@ -92,15 +82,26 @@ for Egg,Data in next,require(game.ReplicatedStorage.Modules.LocalConfig.DrawConf
     end;
 end;
 
-local PetInventory = game:GetService("Players").LocalPlayer.PlayerGui.MainUI.UI.CenterMenu.PetsInventory.PetsContainer.Right.List;
-PetInventory.ChildAdded:Connect(function(child)
+local PetsContainer = game:GetService("Players").LocalPlayer.PlayerGui.MainUI.UI.CenterMenu.PetsInventory.PetsContainer.Right.List;
+PetsContainer.ChildAdded:Connect(function(child)
     if DeleteToggles[child.Info.Power.Text] then
         DeletePet.FireServer(DeletePet,{child.Name});
     end;
 end);
 
+local Size,Signal = #PetsContainer,nil;
+local function ConnectAutoDeleteList(EggList)
+	if #DrawUi:GetChildren()==Size then Signals,Signal=Signal:Disconnect(),nil end;
+    for _,Pet in ipairs(EggList.List.Pets:GetChildren()) do
+        if Pet:IsA("TextButton") and Signals[Pet.Name]~=nil then
+			Signals[Pet.Name]=Signals[Pet.Name]:Disconnect();
+            ToggleAutoPetDelete(Pet.Deleting,List[EggList.Name].List[Pet.Name].Info.Deleting,Abbreviate(PetConfig[Pet.Name].Power));
+        end;
+    end;
+end;
+
 for _,EggList in ipairs(DrawUI:GetChildren()) do
     ConnectAutoDeleteList(EggList)
 end;
 
-DrawUI.ChildAdded:Connect(ConnectAutoDeleteList);
+Signal = DrawUI.ChildAdded:Connect(ConnectAutoDeleteList);
