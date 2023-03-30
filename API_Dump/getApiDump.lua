b023d6300161faae7f46feba5441a5f4f09dfabd
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-03-30 00:36:38 -0400

    Delete getApiDump.lua

--- API_Dump/getApiDump.lua
+++
@@ -1,6 +0,0 @@
--- writefile("rbx_api.dat",game:HttpGet(string.format("http://setup.roblox.com/%s-API-Dump.json", game:HttpGet("http://setup.roblox.com/versionQTStudio"))));
-
-local Version = game:HttpGet("http://setup.roblox.com/versionQTStudio");
-local Dump = "http://setup.roblox.com/%s-API-Dump.json";
-
-writefile("rbx_api.dat",game:HttpGet(string.format(Dump, Version)));
