0fe652d6fd52908c16aa4cde40f4556b5af150f8
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-03-30 16:53:00 -0400

    Update getStuff.lua

--- API_Dump/getStuff.lua
+++ API_Dump/getStuff.lua
@@ -1,14 +1,14 @@
 local api = game:GetService("HttpService"):JSONDecode(readfile("rbx_api.dat"));
-
--- get functions and events
+local find = table.find;
 local functions,events = {},{};
 local f,e = 1,1;
+
 for _,mems in ipairs(api.Classes)do
     for _,dat in ipairs(mems.Members)do
-        if(dat.MemberType=="Function")then
+        if(dat.MemberType=="Function"and not find(functions,dat.Name))then
             functions[f] = dat.Name;
             f += 1;
-        elseif(dat.MemberType=="Event")then
+        elseif(dat.MemberType=="Event"and not find(events,dat.Name))then
             events[e] = dat.Name;
             e += 1;
         end;
@@ -21,7 +21,6 @@ table.sort(events);
 writefile("functions.txt",table.concat(functions,' '));
 writefile("events.txt",table.concat(events,' '));
 
--- get enums
 local enums = {};
 for idx,dat in ipairs(api.Enums)do
     enums[idx] = dat.Name;
