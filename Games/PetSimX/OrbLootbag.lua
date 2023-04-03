b5e81cefd2cf2f2360853bb58e4b227d2c01bd37
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-03 04:03:17 -0400

    Update OrbLootbag.lua

--- Games/PetSimX/OrbLootbag.lua
+++ Games/PetSimX/OrbLootbag.lua
@@ -1,23 +1,31 @@
-
-local LocalPlayer = game:GetService("Players").LocalPlayer
-local PrimaryPart = (LocalPlayer.Character or LpcalPlayer.CharacterAdded:Wait()):WaitForChild("HumanoidRootPart");
-PrimaryPart.Parent.CharacterAdded:Connect(function(c)
-	PrimaryPart = c:WaitForChild("HumanoidRootPart");
-end);
+local wait,PrimaryPart = task.wait,nil;
+do
+	local LocalPlayer = game:GetService("Players").LocalPlayer
+	if(not(LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()).PrimaryPart)then
+		LocalPlayer.Character:GetPropertyChangedSignal("PrimaryPart"):Wait();
+	end;
+	PrimaryPart = LocalPlayer.Character.PrimaryPart;
+	LocalPlayer.CharacterAdded:Connect(function(Character)
+		Character:GetPropertyChangedSignal("PrimaryPart"):Wait();
+		PrimaryPart = Character.PrimaryPart;
+	end);
+end;
 
 local function Sauce(O)
-	while O.Parent ~= nil and task.wait() do
+	while(O.Parent~=nil)do
 		O.CFrame = PrimaryPart.CFrame;
+		wait();
 	end;
 end;
 
-for _,Orb in ipairs(Workspace["__THINGS"].Orbs:GetChildren()) do
-	task.defer(Sauce, Orb);
-end;
+task.defer(function()
+	for _,Orb in ipairs(Workspace.__THINGS.Orbs:GetChildren()) do
+		Sauce(Orb);
+	end;
+	for _,Lootbag in ipairs(Workspace.__THINGS.Lootbags:GetChildren()) do
+		Sauce(Lootbag);
+	end;
+end);
 
-for _,Lootbag in ipairs(Workspace["__THINGS"].Lootbags:GetChildren()) do
-	task.defer(Sauce, Lootbag);
-end;
-
-Workspace["__THINGS"].Orbs.ChildAdded:Connect(Sauce)
-Workspace["__THINGS"].Lootbags.ChildAdded:Connect(Sauce)
+Workspace.__THINGS.Orbs.ChildAdded:Connect(Sauce)
+Workspace.__THINGS.Lootbags.ChildAdded:Connect(Sauce)
