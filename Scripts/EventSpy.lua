da20a9c9e30348cc8064d59702f71fb75fb579a0
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-11 03:58:47 -0400

    Update EventSpy.lua

--- Scripts/EventSpy.lua
+++ Scripts/EventSpy.lua
@@ -1,30 +1,46 @@
-assert(pcall(loadstring(game:HttpGet("https://raw.githubusercontent.com/DoComplement/Roblox/main/Library/TableToString/t2s_v2.lua"))));
+-- simple, but not visually pleasing. only use for learning purposes
+
+assert(loadstring(game:HttpGet("https://raw.githubusercontent.com/DoComplement/Roblox/main/Library/TableToString/t2s_v2.lua")))();
+
 local Services = {};
-local Types = {[1] = "FireServer";[2] = "InvokeServer";};
+local Types = {"FireServer","Fire","InvokeServer","Invoke"};
 local Username = game:GetService("Players").LocalPlayer.Name;
 
 -- Note: service-substitution is only used to indicate the event directory stems from a service (and can be removed via bias)
 
 -- get table of services for substitution
-for _,Instance in ipairs(game:GetChildren()) do
-    if Instance.ClassName == '' then continue; end;-- not tamper proof
-    Services[Instance.Name] = "game:GetService(\""..Instance.ClassName.."\")";
+for _,inst in ipairs(game:GetChildren()) do
+    if(inst.ClassName~='')then  -- not fully operational
+        Services[inst.Name] = "game:GetService(\""..inst.ClassName.."\")";
+    end;
 end;
 
 local function FormatPath(Path)
     return Path:gsub(Username, "LocalPlayer"):gsub("^(%a+)", Services);
 end;
 
-local OldNamecall = nil; -- doesnt provide return value
-OldNamecall = hookmetamethod(game, "__namecall", function(Self, ...)
-    if table.find(Types, getnamecallmethod()) == nil then -- if namecall doesn't indicate a remote
-        return OldNamecall(Self, ...)    
+local onc = nil;onc = hookmetamethod(game, "__namecall", function(self, ...)
+    if(table.find(Types, getnamecallmethod())~=nil)then -- if namecall indicates a remote 
+        print(self.ClassName..":\t", self);
+        print("Reference:\t", FormatPath(self:GetFullName())..':'..getnamecallmethod().."(...)");
+        print("Calling Script:\t", FormatPath((getcallingscript() or script):GetFullName())..'');
+        print(t2s({...}, "Arguments"));
     end;
-    
-    print(Self.ClassName..":\t", Self);
-    print("Reference:\t", FormatPath(Self:GetFullName())..':'..getnamecallmethod().."(...)");
-    print("Calling Script:\t", FormatPath((getcallingscript() or script):GetFullName())..'');
-    print(getgenv().t2s({...}, "Arguments"));
 
-    return OldNamecall(Self, ...)
-end)
+    return onc(self, ...);
+end);
+
+do
+    local functions = {
+        Instance.new("RemoteEvent").FireServer;
+        Instance.new("BindableEvent").Fire;
+        Instance.new("RemoteFunction").InvokeServer;
+        Instance.new("BindableFunction").Invoke;
+    };
+    
+    for _,func in ipairs(functions)do
+        local hook = nil;hook = hookfunction(func, newcclosure(function(remote, ...)
+              -- format data      
+        end));
+    end;
+end;
