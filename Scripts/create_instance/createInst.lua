1da93b5c991e179a50b8dffe6e6990562ae9dd7e
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-11 03:49:45 -0400

    Update createInst.lua

--- Scripts/create_instance/createInst.lua
+++ Scripts/create_instance/createInst.lua
@@ -1,7 +1,8 @@
 -- simple create instance function for code consolidation
 
-local function createInst(class,props)
+local function createInst(class,props,protect)
 	local inst = Instance.new(class);
+	if(protect)then --[[ call respective protect-gui function ]] end;
 	for prop,val in next,props do
 		inst[prop] = val;
 	end;
