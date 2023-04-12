102571ffd27ed51246adde0202019253ff3d768a
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-12 17:06:11 -0400

    Update AutoLevelWeapon.lua

--- Games/SwordSimulator/OtherCode/AutoLevelWeapon.lua
+++ Games/SwordSimulator/OtherCode/AutoLevelWeapon.lua
@@ -5,3 +5,7 @@ local pd = require(game:GetService("ReplicatedStorage").Saturn.Modules.Client["P
 -- weps -> pd.Weapons
 -- equipped weps -> pd.EquippedItems.Weapons
 
+-- example, Exp can be nil if weapon hasn't defeated any mobs
+local function checkWepExp(id, goal)
+    return(pd.Weapons[id].Exp or 0)>=goal;
+end;
