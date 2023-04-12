80ea7599464907dbf71fa9ddefcd3f803497c925
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-12 17:00:12 -0400

    Create AutoLevelWeapon.lua

---
+++ Games/SwordSimulator/OtherCode/AutoLevelWeapon.lua
@@ -0,0 +1,7 @@
+-- get level calculator scripts
+
+local pd = require(game:GetService("ReplicatedStorage").Saturn.Modules.Client["PlayerData - Client"]).Replica.Data.Main;
+
+-- weps -> pd.Weapons
+-- equipped weps -> pd.EquippedItems.Weapons
+
