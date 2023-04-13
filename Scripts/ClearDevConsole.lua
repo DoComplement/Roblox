fecce0b8f8179711cc2ffc2bdfc741e91d4ce4df
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-13 19:46:12 -0400

    Update ClearDevConsole.lua

--- Scripts/ClearDevConsole.lua
+++ Scripts/ClearDevConsole.lua
@@ -1,21 +1,24 @@
 -- messages after each clear will reappear every time the dev console is re-opened
 -- the function can be bound to a keybind to clear the console on some command as well as setting the command to F9 so the console always opens cleared
+if(not game:IsLoaded())then game.Loaded:Wait()end;
 
-local function OpenDevConsole(DevConsoleUI)
-    if DevConsoleUI:FindFirstChild("MainView") ~= nil then
-        return DevConsoleUI.MainView;
+local VirtualUser = game:GetService("VirtualUser");
+local ChatBar = game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar;
+local DevConsoleUi = game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI;
+
+local function OpenDevConsole()
+    if(not DevConsoleUI:FindFirstChild("MainView"))then
+        ChatBar:SetTextFromInput("/console",VirtualUser:CaptureController());
+        VirtualUser:TypeKey("0x0D",ChatBar:CaptureFocus()); -- Press Enter Key
+        DevConsoleUI.ChildAdded:Wait();
     end;
-    local Box = game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar;
-    game:GetService("VirtualUser"):CaptureController();
-    Box:SetTextFromInput("/console");
-    Box:CaptureFocus();
-    game:GetService("VirtualUser"):TypeKey("0x0D"); -- Press Enter Key
-    return DevConsoleUI:WaitForChild("MainView");
+    return game.GetChildren(DevConsoleUI.MainView.ClientLog);
 end;
 
 return function()
-    for _,Instance in ipairs(OpenDevConsole(game:GetService("CoreGui").DevConsoleMaster.DevConsoleWindow.DevConsoleUI).ClientLog:GetChildren()) do
-        if tonumber(Instance.Name) == nil then continue; end;
-        Instance.Visible = false; 
+    for _,inst in ipairs(OpenDevConsole()) do
+        if(tonumber(inst.Name)~=nil)then
+            Instance.Visible = false;
+        end;
     end;
 end;
