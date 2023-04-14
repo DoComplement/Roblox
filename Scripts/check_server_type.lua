346706a993c13f8d648c9b5ce495503a470ee507
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-14 15:59:41 -0400

    Update check_server_type.lua

--- Scripts/check_server_type.lua
+++ Scripts/check_server_type.lua
@@ -1,12 +1,12 @@
 -- method 1
 local function isPrivateServer()
     local data = assert(game:HttpGetAsync("https://httpbin.org/get"),"error obtaining data");
-    data = game:GetService("HttpService"):JSONdecode(data);
+    data = game:GetService("HttpService"):JSONDecode(data);
     local serverType = assert(game:GetService("HttpService"):JSONDecode(data.headers["Roblox-Session-Id"]), "session id not found");
     return serverType.JoinType=="Specific_PrivateServer";
 end;
 
 -- method 2
 local function isPrivateServer()
-    return assert(game:HttpGetAsync("https://httpbin.org/get"),"error obtaining data"):match([[\"JoinType\":\"(%a+)]])=="Specific";
+    return assert(game:HttpGetAsync("https://httpbin.org/get"),"error obtaining data"):match("Specific_PrivateServer")~=nil;
 end;
