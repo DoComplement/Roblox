2891692200d5fd153b6a39bd0060ed6ca811d6e2
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-15 01:47:55 -0400

    Delete Scripts/create_instance directory

--- Scripts/create_instance/create.lua
+++
@@ -1,7 +0,0 @@
-local function setVals(class, props, parent)
-    local inst = Instance.new(class,parent);
-    for prop,val in next,props do
-        inst[prop] = val;
-    end;
-    return inst;
-end;
--- Scripts/create_instance/setVals.lua
+++
@@ -1,9 +0,0 @@
--- somewhat more consolidative "create instance" function
--- setVals(Instance.new("Part", parent), { (properties) });
-
-local function setVals(inst,props)
-    for prop,val in next,props do
-        inst[prop] = val;
-    end;
-    return inst;
-end;
