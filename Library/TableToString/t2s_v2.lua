49d21e1221cc6ca2057db1d948e71080a001701d
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-04-19 14:29:37 -0400

    Update t2s_v2.lua

--- Library/TableToString/t2s_v2.lua
+++ Library/TableToString/t2s_v2.lua
@@ -1,29 +1,28 @@
-if Game:IsLoaded() == false then Game.Loaded:Wait() end
-if t2s then return; end;
+if(t2s~=nil)then return(nil)end;
 
-getgenv().t2s = function(Table, Title, Sort)
-	assert(type(Table) == "table", "ERROR! Input table is not a valid table.");
-    
-	local function GetIndices(Table)
-		local Indices = {}
-        for Index,Value in next, Table do 
-        	table.insert(Indices, Index)
-		end
-        
-		if Sort == nil then return Indices; end;
-		if Sort then
-			table.sort(Indices, function(A,B) return A < B end) -- Ascending
-		else
-			table.sort(Indices, function(A,B) return B < A end) -- Descending
-		end
-        
-		return Indices -- Array and Last Element
-	end
+local function GetIndices(dict,sort)
+	local idxs = {}
+	for idx in next,dict do 
+		idxs[#idxs + 1] = idx;
+	end;
 	
-	local Tables = {};
+	if(sort==nil)then return(idxs)end;
+	if(sort)then
+		table.sort(idxs); 									-- Ascending
+	else
+		table.sort(idxs,function(A,B)return(B<A)end); 		-- Descending
+	end;
+
+	return idxs; 											-- Array
+end;
+
+getgenv().t2s = function(Table, Title, Sort, logID)
+	assert(type(Table) == "table", "ERROR! Input table is not a valid table.");
+	
+	local tbls = {};
 	local function formatTable(Entity, Index, Tab)
-		local Model,T = pcall(type, Entity);
-		if T == "string" then
+		local Model,T = pcall(typeof, Entity);
+		if(T=="string")then
 			Model = '\"'..Entity..'\"';
 		else
 			Model = tostring(Entity);
@@ -37,27 +36,28 @@ getgenv().t2s = function(Table, Title, Sort)
 			Index = '';
 		end;
 		
-		if type(Entity) ~= "table" then 
+		if(type(Entity)~="table")then 
 			return Index..Model..";\n"; 
 		end;
 		
-		local Indices = GetIndices(Entity)
-		if table.find(Tables, Entity) ~= nil then -- checking for repeat-nested tables
+		if(table.find(tbls, Entity)~=nil)then 							-- checking for repeat-nested tables
 			return Index.."\"Repeated Table\",\t-- "..Model..'\n';
 		end;
-			
-		table.insert(Tables, Entity)
-		if table.getn(Indices) == 0 then
-			return Index.."{};\n";
-		end;
+		tbls[#tbls + 1] = Entity;
 		
-		local StringTable = {}
-		for _,Index in ipairs(Indices) do
-			table.insert(StringTable, formatTable(Entity[Index], Index, Tab..'\t'));
+		local idxs = GetIndices(Entity)
+		if(#idxs==0)then return Index.."{};\n"end;
+		
+		local strtbl = {}
+		for _,idx in ipairs(idxs)do
+			strtbl[#strtbl + 1] = formatTable(Entity[idx], idx, Tab..'\t');
 		end;
-		return Index.."{ \t-- "..Model..'\n'..table.concat(StringTable)..Tab.."};\n";
+		if(logID)then
+			return Index.."{ \t-- "..Model..'\n'..table.concat(strtbl)..Tab.."};\n";
+		end;
+		return Index.."{\n"..table.concat(strtbl)..Tab.."};\n";
 	end;
 	
-	return (Title and "local ".. Title.." = " or "return ")..formatTable(Table, nil, '')
+	return(Title and "local ".. Title.." = " or "return ")..formatTable(Table, nil, '')
 end
-print("<string> t2s(<tuple> Table, <string> Title, <boolean> Sort)")
+print("t2s(Table : table, Title : string?, Sort : boolean?, logID : boolean?) : string")
