5ca3563fa9bd8dae174fe4a0101b76e764ba5233
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-05-01 11:59:14 -0400

    Update easter_stuff.lua

--- Games/Miners Haven/easter_stuff.lua
+++ Games/Miners Haven/easter_stuff.lua
@@ -1,44 +1,20 @@
-local hrp = game:GetService("Players").LocalPlayer.Character.PrimaryPart;
+-- somewhat automated. user still has to load a save slot manually because I haven't attempted to spoof the calling env
 
+if(not game:IsLoaded())then game.Loaded:Wait()end;
+if(game.PlaceId~=258258996)then return end;
+
+game:GetService("Players").LocalPlayer.PlayerGui.ChildRemoved:Wait();										-- wait until player is loaded
+local hrp = game:GetService("Players").LocalPlayer.Character.PrimaryPart;
 game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(Character)
     Character:GetPropertyChangedSignal("PrimaryPart"):Wait();
     hrp = Character.PrimaryPart;
 end);
 
 local eggQueue = {};
-local boxQueue = {};
-local a1,a2 = false,false;
-local waitEvent = Instance.new("BindableEvent");
-
-local function tp2Boxes(box,cf)
-    a1 = true;
-    cf = hrp.CFrame;
-    while(#boxQueue>0)do
-        box = table.remove(boxQueue);
-        repeat hrp.CFrame = box.CFrame;
-        until task.wait(0.1)and(box.Transparency~=0 or not(box.Parent));
-    end;
-    hrp.CFrame = cf;
-    a1 = false;
-    waitEvent:Fire();
-end;
-
 
 local f = game:GetService("ReplicatedStorage").EventControllers.Easter.EasterBadgeItem;
 local g = f.Parent.GetEasterEggs;
 local i = f.InvokeServer;
-
-do                                                  -- initialize
-    local ref = game:GetService("Players").LocalPlayer.PlayerGui.GUI.Craftsman.Pages.Easter.Scrolling:GetChildren();
-    if(#ref>2)then
-        local eggs = i(g);                              -- update frame
-        for idx,egg in ipairs(ref)do
-            if(egg.Name=="SampleBadge"and not(egg.Icon.Visible)and eggs[egg.Name]~=nil)then
-                i(f,egg.Name);
-            end;
-        end;
-    end;
-end;
 
 -- on egg claimed
 local function checkEgg(egg)
@@ -47,39 +23,10 @@ local function checkEgg(egg)
     end;
 end;
 
-local function tp2Eggs(egg,cf)
-    a2 = true;
-    cf = hrp.CFrame;
-    while(#eggQueue>0)do
-        egg = table.remove(eggQueue);
-        hrp.CFrame = egg.CFrame;
-        while(task.wait(0.3)and egg.Parent~=nil)do
-            hrp.CFrame = egg.CFrame;
-            fireproximityprompt(egg.ProximityPrompt, 2);
-        end;
-        task.delay(1,checkEgg,egg.Name);
-    end;
-    hrp.CFrame = cf;
-    a2 = false;
-    waitEvent:Fire();
-end;
-
-local function fillBox(box)
-    table.insert(boxQueue,1,box);
-    if(a1)then return end;
-    if(a2)then
-        waitEvent.Event:Wait();
-    end;
-    tp2Boxes();
-end;
-
 local function fillEgg(egg)
-    table.insert(eggQueue,1,egg);
-    if(a2)then return end;
-    if(a1)then
-        waitEvent.Event:Wait();
-    end;
-    tp2Eggs();
+	if(egg~=nil)then
+		table.insert(eggQueue,1,egg);
+	end;
 end;
 
 for _,path in ipairs({Workspace.Easter["EASTER ISLAND EGG SPAWNS"],Workspace.Map.EGG_SPAWNS})do
@@ -89,10 +36,33 @@ for _,path in ipairs({Workspace.Easter["EASTER ISLAND EGG SPAWNS"],Workspace.Map
     end;
 end;
 
-for _,box in ipairs(Workspace.Boxes:GetChildren())do
-    table.insert(boxQueue,1,box);
+do										-- collect easter eggs
+	local egg = nil;
+	cf = hrp.CFrame;
+	while(#eggQueue>0)do
+		egg = table.remove(eggQueue);
+		if(not egg:FindFirstChild("ProximityPrompt"))then continue end;
+		hrp.CFrame = egg.CFrame;
+		task.wait(0.3);
+		repeat fireproximityprompt(egg.ProximityPrompt, 2);
+		until task.wait()and not(egg.Parent);
+		task.delay(1,checkEgg,egg.Name);
+	end;
+	hrp.CFrame = cf;
 end;
 
-Workspace.Boxes.ChildAdded:Connect(fillBox);
-tp2Eggs();
-tp2Boxes();
+do										-- server hop
+	local function getServers()
+		local succ,servers = pcall(game.HttpGetAsync, game, "https://games.roblox.com/v1/games/258258996/servers/0?limit=100&excludeFullGames=true");
+		while(not(succ)and task.wait(5))do
+			succ,servers = pcall(game.HttpGetAsync, game, "https://games.roblox.com/v1/games/258258996/servers/0?limit=100&excludeFullGames=true");
+		end;
+
+		return game:GetService("HttpService"):JSONDecode(servers).data;
+	end;
+
+	local servers = nil;
+	repeat servers = getServers();
+		game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, servers[math.random(#servers)].id);					-- game may crash here occasionally
+	until not task.wait(1);
+end;
