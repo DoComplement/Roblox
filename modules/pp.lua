b25bc3eb3a910ddbec20e07f5a0cde476beb1664
DoComplement <92898998+DoComplement@users.noreply.github.com>
2023-11-28 18:39:17 -0500

    Update pp.lua

--- modules/pp.lua
+++ modules/pp.lua
@@ -1,11 +1,22 @@
 
 -- pp.lua -> get player-parts
 
+if(not game:IsLoaded())then
+	game.Loaded:Wait();
+end;
 if(getgenv()["yo#p^q$XTPCysUGkzO?Y-*A]lBUKTO$m"] ~= nil)then return end;    -- strong pp
 getgenv()["yo#p^q$XTPCysUGkzO?Y-*A]lBUKTO$m"] = true;
 
 local localPlayer = game:GetService("Players").LocalPlayer;
-local character = localPlayer.Character or localPlayer.CharacterAdded:Wait();
+local character = (localPlayer.Character or localPlayer.CharacterAdded:Wait());
+
+local function waitforchild(parent, child)
+	local temp = parent:FindFirstChild(child);
+	while(not temp or temp.Name ~= child)do
+		temp = parent.ChildAdded:Wait();
+	end;
+	return temp;
+end;
 
 local primaryPart = character.PrimaryPart;
 if(not primaryPart)then
@@ -13,14 +24,13 @@ if(not primaryPart)then
 	primaryPart = character.PrimaryPart;
 end;
 
-local humanoid = character:WaitForChild("Humanoid");
+local humanoid = waitforchild(character, "Humanoid");
 
 localPlayer.CharacterAdded:Connect(function(char)
 	char:GetPropertyChangedSignal("PrimaryPart"):Wait();
 	character = char;
 	primaryPart = char.PrimaryPart;
-	humanoid = char:WaitForChild("Humanoid");	-- can yield, easy fix tho
+	humanoid = waitforchild(char, "Humanoid");
 end);
 
-getgenv()["h_parts"] = {character, primaryPart, humanoid};
-return character, primaryPart, humanoid;
+return localPlayer, character, primaryPart, humanoid;
