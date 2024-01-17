7194d0d39357db1ec4fed218548e47ab67e6733e
DoComplement <92898998+DoComplement@users.noreply.github.com>
2024-01-17 12:35:03 -0500

    Update readme.m

--- Games/SwordSimulator/Main/changes_!!/readme.m
+++ Games/SwordSimulator/Main/changes_!!/readme.m
@@ -1,2 +1,3 @@
 changes were made to the format for queuedata files for consolidation reasons. the new format is more legible.
 
+download "json.lua" + "change_format.lua" to the same folder in the ":\...\workspace\SwordSimData\QueueData.json" path then execute change_format.lua and voi·la
