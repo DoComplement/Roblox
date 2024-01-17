51db4d48bf97aa94343f8f74bae26c1d356cbea8
DoComplement <92898998+DoComplement@users.noreply.github.com>
2024-01-17 12:42:46 -0500

    Update change_format.lua

--- Games/SwordSimulator/Main/changes_!!/change_format.lua
+++ Games/SwordSimulator/Main/changes_!!/change_format.lua
@@ -4,8 +4,6 @@ local function rev(a,b)return b,a end;
 local old_fmt = "..., ... %w+, (%S+), (.+)";
 local new_fmt = "%s, %s";
 
--- new_fmt:format(rev(test:match(old_fmt))));
-
 local mt = {
 	__index = function(self, idx)
 		return rawset(self, idx, {})[idx];
@@ -26,7 +24,7 @@ local function insert(tbl, ref, name)
 end;
 
 do
-	local t = require("QueueData_LEGIBLE");
+	local t = assert(require("QueueData_LEGIBLE"), "QueueData_LEGIBLE file not found. Please validate that change_format and QueueData_LEGIBLE are in the same directory");
 	assert(select(2, next(t)).Slots, "error: data already formatted");
 	for name,tbl in next,t do
 		insert(tbl.Pets,	new.Pets,	 name);
